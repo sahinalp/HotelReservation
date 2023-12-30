390fec39ddc31b3fdb88feb9c8aac5bf0d8a57d4
nida <nida8581@gmail.com>
2023-12-30 21:30:05 +0300

    Update Reservation.java
    totalDays is added for reservation

--- src/main/java/com/example/hotelreservation/Entities/Reservation.java
+++ src/main/java/com/example/hotelreservation/Entities/Reservation.java
@@ -8,25 +8,27 @@ public class Reservation implements IEntity{
     private String hotelID;
     private String checkInDate;
     private String checkOutDate;
+    private int totalDays;
 
     public Reservation(String ID, String customerID, String roomID, String hotelID, String checkInDate,
-                       String checkOutDate) {
+                       String checkOutDate,int totalDays) {
         this.ID = ID;
         this.customerID = customerID;
         this.roomID = roomID;
         this.hotelID = hotelID;
         this.checkInDate = checkInDate;
         this.checkOutDate = checkOutDate;
+        this.totalDays = totalDays;
     }
 
     @Override
     public String getValues() {
         return "'"+this.ID+"'"+","+"'"+this.customerID+"'"+","+"'"+this.roomID+"'"+","+
-                "'"+this.hotelID+"'"+","+"'"+this.checkInDate+"'"+","+"'"+this.checkOutDate+"'";
+                "'"+this.hotelID+"'"+","+"'"+this.checkInDate+"'"+","+"'"+this.checkOutDate+"'"+this.totalDays+"'";
     }
 
     @Override
     public String getColumns() {
-        return "ID,customerID,roomID,hotelID,checkInDate,checkOutDate";
+        return "ID,customerID,roomID,hotelID,checkInDate,checkOutDate,totalDays";
     }
 }
