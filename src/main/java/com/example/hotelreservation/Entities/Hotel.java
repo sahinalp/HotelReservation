57a2e0393fbcdec483d2a190c63dfdd98a7d38a2
nida <nida8581@gmail.com>
2023-12-30 21:29:33 +0300

    Update Hotel.java
    city, country and roomCapacity are added for Hotel

--- src/main/java/com/example/hotelreservation/Entities/Hotel.java
+++ src/main/java/com/example/hotelreservation/Entities/Hotel.java
@@ -8,8 +8,11 @@ public class Hotel implements IEntity{
     private String telNo;
     private String fax;
     private Double rank;
+    private String country;
+    private String city;
+    private int roomCapacity;
 
-    public Hotel(String ID, String name, String address, String website, String telNo, String fax, Double rank) {
+    public Hotel(String ID, String name, String address, String website, String telNo, String fax, Double rank,String country,String city,int roomCapacity) {
         this.ID = ID;
         this.name = name;
         this.address = address;
@@ -17,16 +20,19 @@ public class Hotel implements IEntity{
         this.telNo = telNo;
         this.fax = fax;
         this.rank = rank;
+        this.city=city;
+        this.country=country;
+        this.roomCapacity=roomCapacity;
     }
 
     @Override
     public String getValues() {
         return "'"+this.ID+"'"+","+"'"+this.name+"'"+","+"'"+this.address+"'"+","+
-                "'"+this.website+"'"+","+"'"+this.telNo+"'"+","+"'"+this.fax+"'"+","+this.rank;
+                "'"+this.website+"'"+","+"'"+this.telNo+"'"+","+"'"+this.fax+"'"+","+this.rank+","+this.roomCapacity+"'"+this.country+"'"+this.city+"'";
     }
 
     @Override
     public String getColumns() {
-        return "ID,name,address,website,telNo,fax,rank";
+        return "ID,name,address,website,telNo,fax,rank,roomCapacity,country,city,services";
     }
 }
