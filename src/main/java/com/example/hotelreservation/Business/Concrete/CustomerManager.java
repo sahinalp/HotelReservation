57ff7af632dee0ffb6d0563c60216e93971ca436
nida <nida8581@gmail.com>
2023-12-31 22:25:41 +0300

    Create CustomerManager.java

---
+++ src/main/java/com/example/hotelreservation/Business/Concrete/CustomerManager.java
@@ -0,0 +1,22 @@
+package com.example.hotelreservation.Business.Concrete;
+
+import com.example.hotelreservation.Business.Abstract.ICustomerService;
+import com.example.hotelreservation.Entities.Customer;
+
+public class CustomerManager implements ICustomerService {
+
+    @Override
+    public void register() {
+
+    }
+
+    @Override
+    public void updateInfo() {
+
+    }
+
+    @Override
+    public void login() {
+
+    }
+}
