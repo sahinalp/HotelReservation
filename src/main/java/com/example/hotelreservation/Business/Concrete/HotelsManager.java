7f0f3afb5e4a0e1b329fb910c23f5dc79c3c3f59
nida <nida8581@gmail.com>
2023-12-31 22:25:15 +0300

    Update HotelsManager
    methods are added

---
+++ src/main/java/com/example/hotelreservation/Business/Concrete/HotelsManager.java
@@ -0,0 +1,19 @@
+package com.example.hotelreservation.Business.Concrete;
+
+import com.example.hotelreservation.Business.Abstract.IHotelsManager;
+import com.example.hotelreservation.Entities.Customer;
+import com.example.hotelreservation.Entities.Reservation;
+import com.example.hotelreservation.Entities.Room;
+
+class HotelsManager implements IHotelsManager {
+
+    @Override
+    public void getHotels() {
+
+    }
+
+    @Override
+    public void getRooms() {
+
+    }
+}
