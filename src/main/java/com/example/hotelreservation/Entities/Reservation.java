7f352743214883b599df80b298bfe846bb9379d2
nida <nida8581@gmail.com>
2023-12-31 21:45:04 +0300

    Update Reservation.java
    change the type of parameter; customer, room and hotel

--- src/main/java/com/example/hotelreservation/Entities/Reservation.java
+++ src/main/java/com/example/hotelreservation/Entities/Reservation.java
@@ -3,19 +3,19 @@ package com.example.hotelreservation.Entities;
 public class Reservation implements IEntity{
 
     private String ID;
-    private String customerID;
-    private String roomID;
-    private String hotelID;
+    private Customer customer;
+    private Room room;
+    private Hotel hotel;
     private String checkInDate;
     private String checkOutDate;
     private int totalDays;
 
-    public Reservation(String ID, String customerID, String roomID, String hotelID, String checkInDate,
+    public Reservation(String ID, Customer customer, Room room, Hotel hotel, String checkInDate,
                        String checkOutDate,int totalDays) {
         this.ID = ID;
-        this.customerID = customerID;
-        this.roomID = roomID;
-        this.hotelID = hotelID;
+        this.customer = customer;
+        this.room = room;
+        this.hotel = hotel;
         this.checkInDate = checkInDate;
         this.checkOutDate = checkOutDate;
         this.totalDays = totalDays;
@@ -23,12 +23,12 @@ public class Reservation implements IEntity{
 
     @Override
     public String getValues() {
-        return "'"+this.ID+"'"+","+"'"+this.customerID+"'"+","+"'"+this.roomID+"'"+","+
-                "'"+this.hotelID+"'"+","+"'"+this.checkInDate+"'"+","+"'"+this.checkOutDate+"'"+this.totalDays+"'";
+        return "'"+this.ID+"'"+","+"'"+this.customer+"'"+","+"'"+this.room+"'"+","+
+                "'"+this.hotel+"'"+","+"'"+this.checkInDate+"'"+","+"'"+this.checkOutDate+"'"+this.totalDays+"'";
     }
 
     @Override
     public String getColumns() {
-        return "ID,customerID,roomID,hotelID,checkInDate,checkOutDate,totalDays";
+        return "ID,customer,room,hotel,checkInDate,checkOutDate,totalDays";
     }
 }
