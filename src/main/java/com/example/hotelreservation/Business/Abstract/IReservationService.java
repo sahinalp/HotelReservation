f4ce23efd73ffea1b51cb38bc070b79b6dc008b7
nida <nida8581@gmail.com>
2023-12-31 21:22:09 +0300

    Create IReservation.java
    interface is created for ReservationManager

---
+++ src/main/java/com/example/hotelreservation/Business/Abstract/IReservationService.java
@@ -0,0 +1,10 @@
+package com.example.hotelreservation.Business.Abstract;
+
+public interface IReservationService {
+    void makeReservation();
+
+    void cancelRegister();
+
+    void getHistory();
+
+}
