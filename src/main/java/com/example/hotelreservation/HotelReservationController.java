e54ffe040e5b66606da810582f3d4b44f0a9f917
Şahin Alp Akosman <86469980+sahinalp@users.noreply.github.com>
2024-01-09 20:37:21 +0300

    Update UI
    Co-Authored-By: Nida <112012287+NidaSns@users.noreply.github.com>

--- src/main/java/com/example/hotelreservation/HotelReservationController.java
+++ src/main/java/com/example/hotelreservation/HotelReservationController.java
@@ -16,19 +16,26 @@ public class HotelReservationController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Home.fxml"));
-        Parent root1 = null;
-        try {
-            root1 = (Parent) fxmlLoader.load();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    protected void onHelloButtonClick() throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Home.fxml"));
+//        Parent root1 = null;
+//        try {
+//            root1 = (Parent) fxmlLoader.load();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        Stage stage = new Stage();
+//        stage.initModality(Modality.APPLICATION_MODAL);
+//        stage.initStyle(StageStyle.UNDECORATED);
+//        stage.setTitle("ABC");
+//        stage.setScene(new Scene(root1));
+//        stage.show();
+//
         Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.setTitle("ABC");
-        stage.setScene(new Scene(root1));
+        FXMLLoader fxmlLoader = new FXMLLoader(HotelReservationApplication.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Login");
+        stage.setScene(scene);
         stage.show();
     }
 }
